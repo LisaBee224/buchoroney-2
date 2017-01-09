7621b7afeac48d31a227f5ef6e32fc545488f596
LisaBee224 <lisakbuch@gmail.com>
2017-01-08 21:40:19 -0500

    Add fade in and styling

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -2,3 +2,16 @@
 
 
 }).call(this);
+ $(document).ready(function(){
+$("li").on("click", function(e){
+  console.log('click');
+   $(".nav").find(".active").removeClass("active");
+   $(e.target).addClass("active");
+});
+
+
+ $(document).ready(function(){
+  $('.thing').click(function(){
+    console.log("hey");
+  });
+ });

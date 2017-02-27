4216d47c50548f779fd442dd314b8b923d86af8f
Lisa Buch <lisakbuch@gmail.com>
2017-02-27 14:00:21 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  APP_DOMAIN = 'www.leesyintheskywithdavid.com'
-  before_filter :ensure_domain
-
-  def ensure_domain
-    unless request.env['HTTP_HOST'] == APP_DOMAIN || Rails.env.development?
-      redirect_to "http://#{APP_DOMAIN}", :status =&gt; 301
-    end
-  end
   def angular
     @guest = Guest.first
     binding.pry

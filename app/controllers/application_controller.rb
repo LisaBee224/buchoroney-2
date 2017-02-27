02f4dca8ab51bbc69eedac9909fb51ad1a35f813
Lisa Buch <lisakbuch@gmail.com>
2017-02-27 12:45:29 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+  APP_DOMAIN = 'www.leesyintheskywithdavid.com'
+  before_filter :ensure_domain
+
+  def ensure_domain
+    unless request.env['HTTP_HOST'] == APP_DOMAIN || Rails.env.development?
+      redirect_to "http://#{APP_DOMAIN}", :status =&gt; 301
+    end
+  end
   def angular
     @guest = Guest.first
     binding.pry
